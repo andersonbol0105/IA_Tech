e18dcdf2939ca3bfd5b55f6a5905d8cafdd099a7
ANDERSON APARECIDO BOLDINO <anderson.aboldino@senacsp.edu.br>
2025-12-08 11:51:42 -0300

    Ajuste para a coleta de Static

--- IATechProBackend/IATechProBackend/settings.py
+++ IATechProBackend/IATechProBackend/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',   # <--- primeiro
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -146,6 +147,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedMainfestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
